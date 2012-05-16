dd84e98da37ea5b7ac6382fdc49c760fbe697b44
Wolf Vollprecht <w.vollprecht@gmail.com>
2012-05-16 04:33:27 +0200

    initial commit

---
+++ duckduckgo_lens/__init__.py
@@ -0,0 +1,121 @@
+import logging
+import optparse
+
+import gettext
+from gettext import gettext as _
+gettext.textdomain('duckduckgo-lens')
+
+from singlet.lens import SingleScopeLens, IconViewCategory, ListViewCategory
+
+from duckduckgo_lens import duckduckgo_lensconfig
+
+class DuckduckgoLens(SingleScopeLens):
+
+	class Meta:
+		name = 'duckduckgo'
+		description = 'DuckDuckGo Lens'
+		search_hint = 'Search DuckDuckGo'
+		icon = 'duckduckgo.svg'
+		search_on_blank = False
+		category_order = ["results_category", "related_searches", "related_topics"]
+
+	# TODO: Add your categories
+	related_topics = ListViewCategory("Related Topics", 'dialog-information-symbolic')	
+	results_category = ListViewCategory("Related Searches", 'dialog-information-symbolic')
+	related_searches = ListViewCategory("Results", 'dialog-information-symbolic')
+
+	ddg_url = "http://api.duckduckgo.com/"
+
+	def ddg_query(self, search):
+		search = search.replace(" ", "+")
+		search_url = "?q=%s&format=json&no_redirect=1" % search
+		url = self.ddg_url + search_url
+		results = json.loads(urllib2.urlopen(url).read())
+
+		return results
+
+	def search(self, search, results):
+
+		self.ddg_query(search)
+
+		search_results = self.ddg_query(search)
+		
+		if search_results["Redirect"]:
+			parse = urlparse(search_results["Redirect"])
+			host = parse.netloc
+			results.append(
+				search_results["Redirect"],
+				'applications-webbrowsers',
+				self.related_searches,
+				"text/html",
+				"Search " + host + " for:",
+				search,
+				search_results["Redirect"]
+			)
+			return
+			
+		for related in search_results['RelatedTopics']:
+			if "Result" in related:
+				if related["Text"] == 'Category Category':
+					results.append(
+						related['FirstURL'],
+						'/usr/share/unity/lenses/duckduckgo/duckduckgo.svg',
+						self.results_category,
+						"text/html",
+						search,
+						"Category",
+						related['FirstURL'])
+				else:
+					results.append(
+						related['FirstURL'],
+						 '/usr/share/unity/lenses/duckduckgo/duckduckgo.svg',
+						 self.results_category,
+						 "text/html",
+						 "Search for:",
+						 related["Text"],
+						 related['FirstURL'])
+			elif "Topics" in related:
+				name = related["Name"]
+				for result in related["Topics"]:
+					results.append(
+						result['FirstURL'],
+						'/usr/share/unity/lenses/duckduckgo/duckduckgo.svg',
+						self.related_topics,
+						"text/html",
+						name,
+						result["Text"],
+						result["FirstURL"]
+					)
+
+		if search_results["AbstractURL"]:
+			results.append(
+				search_results["AbstractURL"],
+				'applications-webbrowsers',
+				self.related_searches,
+				"text/html",
+				search_results["AbstractSource"],
+				search_results["AbstractText"],
+				search_results["AbstractURL"]
+			)
+
+		if search_results["DefinitionURL"]:
+			results.append(
+				search_results["DefinitionURL"],
+				'applications-webbrowsers',
+				self.related_searches,
+				"text/html",
+				search_results["DefinitionSource"],
+				search_results["Definition"],
+				search_results["AbstractURL"]
+			)
+
+
+		for result in search_results['Results']:
+			results.append(result['FirstURL'],
+						 'applications-webbrowsers',
+						 self.related_searches,
+						 "text/html",
+						 result['Text'],
+						 search_results['AbstractText'],
+						 result['FirstURL'])
+		
---
+++ setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# This file is in the public domain
+### END LICENSE
+
+###################### DO NOT TOUCH THIS (HEAD TO THE SECOND PART) ######################
+
+import os
+import sys
+
+try:
+    import DistUtilsExtra.auto
+    from DistUtilsExtra.command import build_extra
+except ImportError:
+    print >> sys.stderr, 'To build duckduckgo-lens you need https://launchpad.net/python-distutils-extra'
+    sys.exit(1)
+assert DistUtilsExtra.auto.__version__ >= '2.18', 'needs DistUtilsExtra.auto >= 2.18'
+
+def update_config(values = {}):
+
+    oldvalues = {}
+    try:
+        fin = file('duckduckgo_lens/duckduckgo_lensconfig.py', 'r')
+        fout = file(fin.name + '.new', 'w')
+
+        for line in fin:
+            fields = line.split(' = ') # Separate variable from value
+            if fields[0] in values:
+                oldvalues[fields[0]] = fields[1].strip()
+                line = "%s = %s\n" % (fields[0], values[fields[0]])
+            fout.write(line)
+
+        fout.flush()
+        fout.close()
+        fin.close()
+        os.rename(fout.name, fin.name)
+    except (OSError, IOError), e:
+        print ("ERROR: Can't find duckduckgo_lens/duckduckgo_lensconfig.py")
+        sys.exit(1)
+    return oldvalues
+
+
+class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
+    def run(self):
+        values = {'__duckduckgo_lens_data_directory__': "'%s'" % (self.prefix + '/share/duckduckgo-lens/'),
+                  '__version__': "'%s'" % (self.distribution.get_version())}
+        previous_values = update_config(values)
+        DistUtilsExtra.auto.install_auto.run(self)
+        update_config(previous_values)
+
+
+        
+##################################################################################
+###################### YOU SHOULD MODIFY ONLY WHAT IS BELOW ######################
+##################################################################################
+
+DistUtilsExtra.auto.setup(
+    name='duckduckgo-lens',
+    version='0.1',
+    #license='GPL-3',
+    #author='Your Name',
+    #author_email='email@ubuntu.com',
+    #description='UI for managing …',
+    #long_description='Here a longer description',
+    #url='https://launchpad.net/duckduckgo-lens',
+    data_files=[
+        ('share/unity/lenses/duckduckgo', ['duckduckgo.lens']),
+        ('share/dbus-1/services', ['unity-lens-duckduckgo.service']),
+        ('share/unity/lenses/duckduckgo', ['unity-lens-duckduckgo.svg']),
+        ('share/unity/lenses/duckduckgo', ['duckduckgo.svg']),
+        ('bin', ['bin/duckduckgo-lens']),
+    ],
+    cmdclass={"build":  build_extra.build_extra, 'install': InstallAndUpdateDataDirectory}
+    )
+
